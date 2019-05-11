64b4306a8e1a462f604f4d0f9552749ebb83b780
Joseph Foley <josephfoley@rcn.com>
2019-05-11 12:32:49 -0400

    Add files via upload

---
+++ fizzbuzz.py
@@ -0,0 +1,17 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Joseph Foley
+#
+# Created:     11/05/2019
+# Copyright:   (c) Joseph Foley 2019
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+for i in range(1, 25):
+    if i%2==0:
+        print("fizz")
+    elif i%2 != 0:
+        print("buzz")
+
